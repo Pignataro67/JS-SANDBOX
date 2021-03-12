f4060328002f06c81f3863d68356a774cebf0460
Pignataro67 <Pignataro67@gmail.com>
2021-03-12 08:47:37 -0800

    JS practice/refresher before algorithim practice!

---
+++ app.js
@@ -0,0 +1,31 @@
+// Log to console
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.log([1, 2, 3, 4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+
+// console.error('This is some error');
+// console.clear();
+
+/*
+  multi
+  line 
+  comments
+*/
+
+// var, let, const
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Init var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
