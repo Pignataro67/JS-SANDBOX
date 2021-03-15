c7e7d36fda2b74a30f53a95a58b4d5fe1f4c1370
Pignataro67 <Pignataro67@gmail.com>
2021-03-15 11:07:35 -0700

    adding Primitve types, more practice before Algosgit add .

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
 // letters, numbers, -, $
 // can't start with a number
-var _name = 'John'
+// var _name = 'John'
 
 // Multi word vars
-var firstName = 'John'; // Camel Case
-var first_name = 'Sara'; // Underscore
-var FirstName = 'Tim'; // Pascal case
-var firstname; // not recommended
+// var firstName = 'John'; // Camel Case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tim'; // Pascal case
+// var firstname; // not recommended
 
 // LET
 
-let name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
 
 // CONST
-const name = 'John';
-console.log(name);
+// const name = 'John';
+// console.log(name);
 // Can't reassign
-name = 'Sara';
+// name = 'Sara';
 // Have to assign value
-const greeting;
+// const greeting;
 
 const person = {
   name: 'John',
   age: 30
 }
 
-console.log(person);
+person.name = 'Sally';
+person.age = '32';
+
+// console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+numbers = [1, 2, 3, 4, 5, 6]
+
+// console.log(numbers);
+
+// PRIMITIVE
+
+// String
+const name = 'John Doe';
+
+const age = 45;
+
+const hasKids = true;
+
+const car = null;
+
+let test;
+
+const sym = Symbol();
+
+console.log(typeof sym);
