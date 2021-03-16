cdc4c51c1250c24c05dda91791abf824262f0387
Pignataro67 <Pignataro67@gmail.com>
2021-03-16 07:20:00 -0700

    type conversion practice before algo practice!

--- app.js
+++ app.js
@@ -72,8 +72,31 @@ let val;
 // Number to string
 val = String(555);
 val = String(4 + 4);
+// Boolean to string
+val = String(true);
+// Date to string
+val = String(new Date());
+// Array to string
+val = String([1,2,3,4]);
+val = (5).toString();
+val = (true).toString();
+
+// toString
+val = (5).toString();
+val = (true).toString();
+
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1,2,3]);
+
+val = parseInt('100.30');
 
 // Output
 console.log(val);
 console.log(typeof val);
-console.log(val.length);
+// console.log(val.length);
+console.log(val.toFixed());
