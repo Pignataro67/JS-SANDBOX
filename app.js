69e43a006f8a560d6f8caf2c63715182c9316613
Pignataro67 <Pignataro67@gmail.com>
2021-03-16 09:59:37 -0700

    numbers and Math(Object) practice

--- app.js
+++ app.js
@@ -1,47 +1,3 @@
-// console.log(typeof today);
-
-let val;
-
-// Number to string
-val = String(555);
-val = String(4 + 4);
-// Boolean to string
-val = String(true);
-// Date to string
-val = String(new Date());
-// Array to string
-val = String([1,2,3,4]);
-val = (5).toString();
-val = (true).toString();
-
-// toString
-val = (5).toString();
-val = (true).toString();
-
-// String to number
-val = Number('5');
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number('hello');
-val = Number([1,2,3]);
-
-val = parseInt('100.30');
-val = parseFloat('100.30');
-
-// Output
-//console.log(val);
-//console.log(typeof val);
-// console.log(val.length);
-//console.log(val.toFixed());
-
-const val1 = String(5);
-const val2 = 6;
-const sum = Number(val1 + val2);
-
-console.log(sum);
-console.log(typeof sum);
-
 const num1 = 100;
 const num2 = 50;
 let val3;
@@ -52,5 +8,20 @@ val3 = num1 * num2
 val3 = num1 - num2
 val3 = num1 / num2
 val3 = num1 % num2
+
+// Math Object
+val3 = Math.PI;
+val3 = Math.E;
+val3 = Math.round(2.4)
+val3 = Math.ceil(2.4);
+val3 = Math.floor(2.8);
+val3 = Math.sqrt(64);
+val3 = Math.abs(-3);
+val3 = Math.pow(8, 2);
+val3 = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+val3 = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+val3 = Math.random(); 
+
+val3 = Math.floor(Math.random() * 20 + 1);
 
 console.log(val3);
