2fab4d945d29050bef3e0f90041cef6c32735823
Pignataro67 <Pignataro67@gmail.com>
2021-03-17 05:01:52 -0700

    Concatenation/Append refresher

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-const num1 = 100;
-const num2 = 50;
-let val3;
+const firstName = 'William';
+const lastName = 'Johnson';
 
-// Simple Math with numbers
-val3 = num1 + num2
-val3 = num1 * num2
-val3 = num1 - num2
-val3 = num1 / num2
-val3 = num1 % num2
+let val;
 
-// Math Object
-val3 = Math.PI;
-val3 = Math.E;
-val3 = Math.round(2.4)
-val3 = Math.ceil(2.4);
-val3 = Math.floor(2.8);
-val3 = Math.sqrt(64);
-val3 = Math.abs(-3);
-val3 = Math.pow(8, 2);
-val3 = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
-val3 = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
-val3 = Math.random(); 
+val = firstName + LastName;
 
-val3 = Math.floor(Math.random() * 20 + 1);
+// Concatenation
+val = firstName + ' ' + lastName;
 
-console.log(val3);
+// Append
+val = 'Pat ';
+val += 'Pignataro';
+
+console.log(val);
