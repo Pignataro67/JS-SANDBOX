b7043925ffbde84c644f28c7d8d0339d0b2997b4
Pignataro67 <Pignataro67@gmail.com>
2021-03-17 05:22:33 -0700

    Methods and Concatenation refresher

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const firstName = 'William';
 const lastName = 'Johnson';
+const age = 42;
 
 let val;
 
-val = firstName + LastName;
+val = firstName + lastName;
 
 // Concatenation
 val = firstName + ' ' + lastName;
@@ -11,5 +12,22 @@ val = firstName + ' ' + lastName;
 // Append
 val = 'Pat ';
 val += 'Pignataro';
+
+val = 'Hello my name is ' + firstName + ' and I am ' + age;
+
+// Escaping
+val = 'That\'s awesome, I can\'t wait';
+
+// Length
+val = firstName.length;
+
+// concat()
+val = firstName.concat(' ', lastName);
+
+// Change Case
+val = firstName.toUpperCase();
+val = firstName.toUpperCase();
+
+val = firstName[0];
 
 console.log(val);
