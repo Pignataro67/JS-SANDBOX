a0d6064ab063cf9ea186a14155ee4e6b1ef67100
Pignataro67 <Pignataro67@gmail.com>
2021-03-22 08:41:41 -0700

    mo' object literals practice

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const person = {
   firstName: 'Steve',
   lastName: 'Smith',
-  age: 30,
+  age: 35,
   email: 'steve@yahoo.com',
   hobbies: ['music', 'sports'],
-  adress: {
-    city: 'Sante Fe'
+  address: { 
+    city: 'Sante Fe', 
+    state: 'New Mexico'
+  },
+  getBirthYear: function() {
+    return 2017 - this.age;
   }
 }
 
@@ -15,5 +19,20 @@ val = person;
 // Get specific value
 val = person.firstName;
 val = person['lastName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address.city;
+val = person. getBirthYear();
 
 console.log(val);
+
+const people = [
+    {name: 'John', age:30},
+    {name: 'Mike', age:23},
+    {name: 'Nancy', age: 40}
+];
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
