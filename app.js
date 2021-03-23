2e87cbac5db5600f485a69bb9e70a6c6fc73c95a
Pignataro67 <Pignataro67@gmail.com>
2021-03-23 05:28:37 -0700

    more if statements and comparison operators

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// if(something) {
-//   do something
-// } else {
-//   do something else
-// }
+// // if(something) {
+// //   do something
+// // } else {
+// //   do something else
+// // }
 
-const id = 100;
+const id = '200';
 
 // Equal to
 if(id == 100) {
@@ -25,4 +25,50 @@ if(id === 100) {
   console.log('Correct');
 } else {
   console.log('Incorrect');
+}
+
+// Equal to Vallue and Type
+if(id !== 100) {
+  console.log('Correct');
+} else {
+  console.log('Incorrect');
+}
+
+// Test if undefined
+if (typeof id !== 'undefined') {
+  console.log(`The ID is ${id}`);
+} else {
+  console.log('NO ID');
+}
+
+// Greater or Less Than
+if(id <= 100) {
+  console.log('Correct');
+} else {
+  console.log('Incorrect');
+}
+
+// If Else
+
+const color = 'yellow';
+
+if(color === 'red') {
+  console.log('Color is red');
+} else if(color === 'blue') {
+  console.log('Color is blue');
+} else {
+  console.log('Color is not red or blue');
+}
+
+// Logical Operators
+
+const name = 'Steve';
+const age = 20;
+
+if (age > 0 && age < 12) {
+    console.log(`${name} is a child`);
+} else if (age>= 13 && age <= 19) {
+  console.log(`${name} is a teenager`);
+} else {
+  console.log(`${name} is an adult`);
 }
