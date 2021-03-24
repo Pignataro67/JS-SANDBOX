2007e16e29b99c469464394b025b01a1c6f7b533
Pignataro67 <Pignataro67@gmail.com>
2021-03-24 05:03:20 -0700

    Function  Declarations and Expressions

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-const color = 'red';
+// Function Declarations
 
-switch(color) {
-  case 'red':
-    console.log('Color is red');
-    break;
-  case 'blue':
-    console.log('Color is blue');
-    break;
-  default:
-    console.log('Color is not red or blue');
-    break;
+function greet(firstName = 'John', lastName = 'Doe') {
+//   if(typeof firstName === 'undefined')(firstName = 'John')
+//   if(typeof lastName === 'undefined')(lastName = 'Doe')
+//  console.log('Hello');
+return 'Hello ' + firstName + ' ' + lastName;
 }
 
-let day; 
+// console.log(greet());
 
-switch(new Date().getDay()) {
-  case 0:
-    day= 'Sunday';
-    break;
-  case 1:
-    day= 'Monday';
-    break;
-  case 2:
-    day= 'Tuesday';
-    break;
-  case 3:
-    day= 'Wednesday';
-    break;
-  case 4:
-    day= 'Thursday';
-    break;
-  case 5:
-    day= 'Friday';
-    break;
-  case 6:
-    day= 'Saturday';
-    break;
-}
+// Function Expression
 
-console.log(`Today is ${day}`);
+const square = function(x = 3) {
+  return x*x;
+};
+
+console.log(square());
