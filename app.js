2e3722ef2127f59125fb72ac3db5fe5c579b8967
Pignataro67 <Pignataro67@gmail.com>
2021-03-24 04:38:33 -0700

    more if statements and comparison operators

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // //   do something else
 // // }
 
-const id = '200';
+const id = 100;
 
 // Equal to
 if(id == 100) {
@@ -63,12 +63,29 @@ if(color === 'red') {
 // Logical Operators
 
 const name = 'Steve';
-const age = 20;
+const age = 4;
 
+// AND &&
 if (age > 0 && age < 12) {
     console.log(`${name} is a child`);
-} else if (age>= 13 && age <= 19) {
+} else if (age >= 13 && age <= 19) {
   console.log(`${name} is a teenager`);
 } else {
   console.log(`${name} is an adult`);
 }
+
+// OR ||
+if(age < 16 || age > 65) {
+  console.log(`${name} can not run in the race`);
+} else {
+  console.log(`${name} is registered for the race`);
+}
+
+// Ternary Operator
+console.log(id === 100 ? 'Correct' : 'Incorrect');
+
+// Without Braces
+if(id === 100)
+  console.log('Correct');
+else
+  console.log('Incorrect');
