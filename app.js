28a8cb8b3cb0043163a61541e0e968960d8d16a7
Pignataro67 <Pignataro67@gmail.com>
2021-03-25 05:54:38 -0700

    Loop Practice

--- app.js
+++ app.js
@@ -1,47 +1,15 @@
-// Function Declarations
+// For Loop
 
-function greet(firstName = 'John', lastName = 'Doe') {
-//   if(typeof firstName === 'undefined')(firstName = 'John')
-//   if(typeof lastName === 'undefined')(lastName = 'Doe')
-//  console.log('Hello');
-return 'Hello ' + firstName + ' ' + lastName;
-}
-
-// console.log(greet());
-
-// Function Expression
-
-const square = function(x = 3) {
-  return x*x;
-};
-
-// console.log(square());
-
-// Immediately Invokable Functio Expressions - IIFEs
-
-// (function() {
-//   console.log('IIFE ran..');
-// })();
-
-// (function(name) {
-//   console.log('Hello ' + name);
-// })('Pat');
-
-// Property Methods
-
-const todo = { 
-  add: function() {
-    console.log('Add todo..');
-  },
-  edit: function(id) {
-    console.log(`Edit todo ${id}`)
+for(let i = 0; i < 10; i++) {
+  // console.log('Number ' + i);
+  if(i ===2) {
+    console.log('2 is my favorite number');
+    continue;
   }
+  
+  if(i === 5) {
+    console.log('Stop the loop');
+    break;
+  }
+  console.log('Number ' + i);
 }
-
-todo.delete = function() {
-  console.log('Delete todo...');
-}
-
-todo.add();
-todo.edit(22);
-todo.delete();
