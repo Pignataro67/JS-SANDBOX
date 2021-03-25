0412279b843f37b0c8765a23df0fba843e05fd24
Pignataro67 <Pignataro67@gmail.com>
2021-03-25 05:48:40 -0700

    Function declarations and expressions

--- app.js
+++ app.js
@@ -15,4 +15,33 @@ const square = function(x = 3) {
   return x*x;
 };
 
-console.log(square());
+// console.log(square());
+
+// Immediately Invokable Functio Expressions - IIFEs
+
+// (function() {
+//   console.log('IIFE ran..');
+// })();
+
+// (function(name) {
+//   console.log('Hello ' + name);
+// })('Pat');
+
+// Property Methods
+
+const todo = { 
+  add: function() {
+    console.log('Add todo..');
+  },
+  edit: function(id) {
+    console.log(`Edit todo ${id}`)
+  }
+}
+
+todo.delete = function() {
+  console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
