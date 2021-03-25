147164898967a2e545b7caf2e3167fdc9fb0c9a0
Pignataro67 <Pignataro67@gmail.com>
2021-03-25 06:11:39 -0700

    more Loop Practice, running in console, previous loops are commented out

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
 // For Loop
 
-for(let i = 0; i < 10; i++) {
-  // console.log('Number ' + i);
-  if(i ===2) {
-    console.log('2 is my favorite number');
-    continue;
-  }
+// for(let i = 0; i < 10; i++) {
+//   // console.log('Number ' + i);
+//   if(i === 2) {
+//     console.log('2 is my favorite number');
+//     continue;
+//   }
   
-  if(i === 5) {
-    console.log('Stop the loop');
-    break;
-  }
-  console.log('Number ' + i);
+//   if(i === 5) {
+//     console.log('Stop the loop');
+//     break;
+//   }
+//   console.log('Number ' + i);
+// }
+
+// While Loop
+
+// let i = 0;
+
+// while(i < 10) {
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// Do While
+
+// let i = 0;
+
+// do {
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// while(i < 10);
+
+// let i = 100;
+
+// do {
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// while(i < 10);
+
+const cars = ['Ford', 'Chevrolet', 'Honda', 'Toyota'];
+// cars[1];
+
+for(let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
 }
