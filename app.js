42ca24554500a6e93080365d7d7c46186430d34b
Pignataro67 <Pignataro67@gmail.com>
2021-03-26 05:36:41 -0700

    mo' Window methods, previous methods are commented out for interpretation

--- app.js
+++ app.js
@@ -18,5 +18,21 @@ let val;
 
 // Outer height and width
 val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll Points
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
 
 console.log(val);
