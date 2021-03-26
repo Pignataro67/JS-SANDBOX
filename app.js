6b7accfeb0137e9a4c81c7e887c1cec225177b1c
Pignataro67 <Pignataro67@gmail.com>
2021-03-26 04:50:26 -0700

    even more Loop Practice, running in console, previous loops are commented out

--- app.js
+++ app.js
@@ -49,3 +49,34 @@ const cars = ['Ford', 'Chevrolet', 'Honda', 'Toyota'];
 for(let i = 0; i < cars.length; i++) {
   console.log(cars[i]);
 }
+
+// ForEach
+cars.forEach(function(car, index, array) {
+  console.log(`${index} : ${car}`);
+  console.log(array);
+});
+
+// Map
+const users = [
+  {id:1, name:'John'},
+  {id:2, name:'Sara'},
+  {id:3, name:'Karen'},
+  {id:4, name:'Steve'}
+];
+
+const ids = users.map(function(user) {
+  return user.id;
+});
+
+console.log(ids);
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 40
+}
+
+// FOr in loop
+for(let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
