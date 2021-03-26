d98f60ee0add14b48e4424d9bc49c22e8f42ece6
Pignataro67 <Pignataro67@gmail.com>
2021-03-26 05:01:57 -0700

    Window methods, previous methods are commented out for interpretation

--- app.js
+++ app.js
@@ -1,82 +1,22 @@
-// For Loop
+// Window Methods / Objects / Properties
 
-// for(let i = 0; i < 10; i++) {
-//   // console.log('Number ' + i);
-//   if(i === 2) {
-//     console.log('2 is my favorite number');
-//     continue;
-//   }
-  
-//   if(i === 5) {
-//     console.log('Stop the loop');
-//     break;
-//   }
-//   console.log('Number ' + i);
+// Alert
+// alert('Hello World');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if (confirm('Are you sure')) {
+//   console.log('Yes');
+// } else {
+//   console.log('No');
 // }
 
-// While Loop
+let val;
 
-// let i = 0;
+// Outer height and width
+val = window.outerHeight;
 
-// while(i < 10) {
-//   console.log('Number ' + i);
-//   i++;
-// }
-
-// Do While
-
-// let i = 0;
-
-// do {
-//   console.log('Number ' + i);
-//   i++;
-// }
-
-// while(i < 10);
-
-// let i = 100;
-
-// do {
-//   console.log('Number ' + i);
-//   i++;
-// }
-
-// while(i < 10);
-
-const cars = ['Ford', 'Chevrolet', 'Honda', 'Toyota'];
-// cars[1];
-
-for(let i = 0; i < cars.length; i++) {
-  console.log(cars[i]);
-}
-
-// ForEach
-cars.forEach(function(car, index, array) {
-  console.log(`${index} : ${car}`);
-  console.log(array);
-});
-
-// Map
-const users = [
-  {id:1, name:'John'},
-  {id:2, name:'Sara'},
-  {id:3, name:'Karen'},
-  {id:4, name:'Steve'}
-];
-
-const ids = users.map(function(user) {
-  return user.id;
-});
-
-console.log(ids);
-
-const user = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 40
-}
-
-// FOr in loop
-for(let x in user) {
-  console.log(`${x} : ${user[x]}`);
-}
+console.log(val);
