a6be46404c94dd94a547cf87fe79b801863c7ec1
Pignataro67 <Pignataro67@gmail.com>
2021-03-29 05:30:40 -0700

    adding the window object

--- app.js
+++ app.js
@@ -35,4 +35,23 @@ val = window.location.port;
 val = window.location.href;
 val = window.location.search;
 
+// Redirect
+// window.location.href = 'https://google.com';
+// Reload
+// window.location.reload();
+
+// History Object
+
+// window.history.go(-2);
+// val =window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
 console.log(val);
