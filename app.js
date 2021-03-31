fe27d9eac4854246fefcf1f1bea52b044f3f0a6a
Pignataro67 <Pignataro67@gmail.com>
2021-03-31 08:11:55 -0700

    mo' document object practice

--- app.js
+++ app.js
@@ -8,8 +8,25 @@ val = document.head;
 val = document.body;
 val = document.doctype;
 val = document.domain;
-val = docment.URL;
+val = document.URL;
 val = document.characterSet;
 val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val = documment.links;
+val = documment.links[0];
+val = documents.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.images;
+
+val = document.scripts;
+val = documment.scripts[2].getAttributes('src');
 
 console.log(val);
