7c73f82560f579e862f424eb13461aa1fe47bebd
Pignataro67 <Pignataro67@gmail.com>
2021-03-31 06:02:16 -0700

    document object practice

---
+++ app.js
@@ -0,0 +1,15 @@
+let val;
+
+val = document;
+val = document.all;
+val = document.all[0];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = docment.URL;
+val = document.characterSet;
+val = document.contentType;
+
+console.log(val);
