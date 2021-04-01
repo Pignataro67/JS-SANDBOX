2feade50d75a3d588cf16dcfe633d9dc10f3cc61
Pignataro67 <Pignataro67@gmail.com>
2021-04-01 05:41:05 -0700

    refract. for effeciency, mo' document selector practice

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ console.log(document.getElementById('task-title'));
 console.log(document.getElementById('task-title').id);
 console.log(document.getElementById('task-title').className);
 
+const taskTitle = document.getElementById('task-title');
+
 // Change Styling 
-document.getElementById('task-title').style.background = '#333";'
-document.getElementById('task-title').style.color = '#fff';
-document.getElementById('task-title').style.padding = '5px';
+taskTitle.style.background = '#333';
+taskTitle.style.color = '#fff';
+taskTitle.style.padding = '5px';
 // document.getElementById('task-title').style.display = 'none';
+
+// Change content
+taskTitle.textContent = 'Task List';
+taskTitle.innerText = 'My Tasks';
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+// document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('#card-title'));
+console.log(document.querySelector('h5'));
