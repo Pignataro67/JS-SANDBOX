a94d8b39ac58c595e0bc64fec11b87253f9db4bc
Pignataro67 <Pignataro67@gmail.com>
2021-04-02 04:14:55 -0700

    mo' dom multiple element selector practice

--- app.js
+++ app.js
@@ -16,3 +16,24 @@ console.log(lis);
 console.log(lis[0]);
 lis[0].style.color = 'red';
 lis[3].textContent = 'Hello';
+
+// Convert HTML Collection into array
+lis = Array.from(lis);
+
+lis.reverse();
+
+lis.forEach(function(li, index) {
+  console.log(li.className);
+  li.textContent = `${index}: Hello`;
+});
+
+console.log(lis);
+
+// document.querySelectorAll
+const items = document.querySelectorAll('ul.collection li.colllection-item');
+
+items.forEach(function(item, index) {
+  item.textContent = `${index}: Hello`;
+});
+
+console.log(items);
