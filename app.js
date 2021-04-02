0abb9ccdabe8aab054f3de2a07b181d94838ae93
Pignataro67 <Pignataro67@gmail.com>
2021-04-02 04:19:41 -0700

    mo' dom multiple element selector practice

--- app.js
+++ app.js
@@ -36,4 +36,15 @@ items.forEach(function(item, index) {
   item.textContent = `${index}: Hello`;
 });
 
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+liOdd.forEach(function(li, index) {
+  li.style.background = '#ccc';
+});
+
+for(let i = 0; i < liEven.length; i++) {
+  liEven[i].style.background = '#f4f4f4';
+}
+
 console.log(items);
