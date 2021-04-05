e615f1933e15579e1821106b043cce5666cbd6f4
Pignataro67 <Pignataro67@gmail.com>
2021-04-05 05:37:47 -0700

    working with the DOM

--- app.js
+++ app.js
@@ -9,4 +9,23 @@ val = list;
 // Get child nodes
 val = list.childNodes;
 
+val = list.chilNodes[0];
+val = list.chilNodes[0].nodeName;
+val = list.chilNodes[3].nodeType;
+
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+
+// Get children element nodes
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = 'Hello';
+// Children of children
+val = list.children[3].children;
+
 console.log(val);
