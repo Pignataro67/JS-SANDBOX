7824058a550883ecd246b60faf329cafe0b2293b
Pignataro67 <Pignataro67@gmail.com>
2021-04-05 08:05:17 -0700

    working with the DOM

--- app.js
+++ app.js
@@ -26,6 +26,29 @@ val = list.children;
 val = list.children[1];
 list.children[1].textContent = 'Hello';
 // Children of children
-val = list.children[3].children;
+val = list.children[3].children[0];
 
+// First Child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Last child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Count child elements
+val = list.childElementCount;
+
+// Get Parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+// Get prev sibling
+val = listItem.previousSibling;
+val = listItem.previousElementSibling;
 console.log(val);
