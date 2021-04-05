c6f1a580d9738e21f5a3acf32430c3ce4ffcd9e6
Pignataro67 <Pignataro67@gmail.com>
2021-04-05 05:37:19 -0700

    adding variables, working with the DOM

--- app.js
+++ app.js
@@ -1,50 +1,12 @@
-// document.getElementsByClassName
+let val;
 
-// const items = document.getElementsByClassName('collection-item');
-// console.log(items);
-// console.log(items[0]);
-// items[0].style.color = 'red';
-// items[3].textContent = 'Hello';
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
 
-// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+val = listItem;
+val = list;
 
-// console.log(listItems);
+// Get child nodes
+val = list.childNodes;
 
-// document.getElementsByTagName
-const lis = document.getElementsByTagName('li');
-console.log(lis);
-console.log(lis[0]);
-lis[0].style.color = 'red';
-lis[3].textContent = 'Hello';
-
-// Convert HTML Collection into array
-lis = Array.from(lis);
-
-lis.reverse();
-
-lis.forEach(function(li, index) {
-  console.log(li.className);
-  li.textContent = `${index}: Hello`;
-});
-
-console.log(lis);
-
-// document.querySelectorAll
-const items = document.querySelectorAll('ul.collection li.colllection-item');
-
-items.forEach(function(item, index) {
-  item.textContent = `${index}: Hello`;
-});
-
-const liOdd = document.querySelectorAll('li:nth-child(odd)');
-const liEven = document.querySelectorAll('li:nth-child(even)');
-
-liOdd.forEach(function(li, index) {
-  li.style.background = '#ccc';
-});
-
-for(let i = 0; i < liEven.length; i++) {
-  liEven[i].style.background = '#f4f4f4';
-}
-
-console.log(items);
+console.log(val);
