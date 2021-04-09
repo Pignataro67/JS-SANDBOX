8e29be35dbcf2ac1623c28a5fe015518236403b7
Pignataro67 <Pignataro67@gmail.com>
2021-04-09 05:21:05 -0700

    more Input Events

--- app.js
+++ app.js
@@ -1,48 +1,32 @@
-const clearBtn = document.querySelector('.clear-tasks');
-const card = document.querySelector('.card');
-const heading = document.querySelector('h5');
-
-// Click
-// clearBtn.addEventListener('click', runEvent);
-// DoubleClick
-// clearBtn.addEventListener('dbclick', runEvent);
-// MouseDown
-// clearBtn.addEventListener('mousedown', runEvent);
-// MouseUp
-// clearBtn.addEventListener('mouseUp', runEvent);
-// Mouseenter
-card.addEventListener('mouseenter', runEvent);
-// Mouseleave
-card.addEventListener('mouseleave', runEvent);
-// Mouseover
-card.addEventListener('mouseover', runEvent);
-// Mouseout
-card.addEventListener('mouseout', runEvent);
-// Mousemove
-card.addEventListener('mousemove', runEvent);
-
-// Event Handler
-function runEvent(e) {
-  console.log(`EVENT TYPE: ${e.type}`);
-
-  heading.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
-
-  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
-}
-
 const form = document.querySelector('form');
 const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
 
 // Clear input
 taskInput.value = '';
 
-form.addEventListener('submit', runEvent);
+// form.addEventListener('submit', runEvent);
+
+// keydown
+// taskInput.addEventListener('keydown', runEvent);
+// keyup
+// taskInput.addEventListener('keyup', runEvent);
+// keypress
+// taskInput.addEventListener('keypress', runEvent);
+// Focus
+taskInput.addEventListener('focus', runEvent);
+// Blur
+taskInput.addEventListener('blur', runEvent);
 
 function runEvent(e) {
   console.log(`EVENT TYPE: ${e.type}`);
 
-  // Get input value
-  console.log(taskInput.value);
+  console.log(e.target.value);
 
-  e.preventDefault();
+  // heading.innerText = e.target.value;
+
+  // Get input value
+  // console.log(taskInput.value);
+
+  // e.preventDefault();
 }
