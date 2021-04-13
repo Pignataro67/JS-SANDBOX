d06f60ca4dbe38afa8fdd6fd1479f275c746f403
Pignataro67 <Pignataro67@gmail.com>
2021-04-13 05:05:03 -0700

    local and session storage, using comments to run function

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 // set local storage item
-localStorage.setItem('name', 'John');
+// localStorage.setItem('name', 'John');
+// localStorage.setItem('age', '30');
 
-// set session storage item
-sessionStorage.setItem('name', 'Beth');
+// // set session storage item
+// sessionStorage.setItem('name', 'Beth');
 
-// remove from Storage
-localStorage.removeItem('name');
+// // remove from Storage
+// localStorage.removeItem('name');
 
-// get from Sotrage
-const name = localSotrage.getItem('name');
-console.log(name);
+// // get from Storage
+// const name = localSotrage.getItem('name');
+// const age = localSotrage.getItem('age');
+
+// // clear local storage
+// localStorage.clear();
+
+// console.log(name, age);
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  const task = document.getElementById('task').value;
+  localStorage.setItem('task', task);
+  alert('Task saved');
+
+  e.preventDefault();
+});
