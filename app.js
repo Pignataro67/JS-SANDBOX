5326717308aa6a22a03443cc606b299866337457
Pignataro67 <Pignataro67@gmail.com>
2021-04-13 04:58:57 -0700

    local and session storage

--- app.js
+++ app.js
@@ -1,41 +1,12 @@
-// // Event Bubbling
+// set local storage item
+localStorage.setItem('name', 'John');
 
-// document.querySelector('.card-title').addEventListener('click', function() {
-//   console.log('card-title');
-// });
+// set session storage item
+sessionStorage.setItem('name', 'Beth');
 
-// document.querySelector('.card-content').addEventListener('click', function() {
-//   console.log('card-content');
-// });
+// remove from Storage
+localStorage.removeItem('name');
 
-// document.querySelector('.card').addEventListener('click', function() {
-//   console.log('card');
-// });
-
-// document.querySelector('.col').addEventListener('click', function() {
-//   console.log('col');
-// });
-
-// Event Delegation
-
-// const delItem = document.querySelector('.delete-item');
-
-// delItem.addEventListener('click', deleteItem);
-
-document.body.addEventListener('click', deleteItem);
-
-function deleteItem(e) {
-  
-  // console.log(e.target);
-  // if (e.target.className === 'fa fa-remove') {
-  // console.log('delete item');
-    
-  // if (e.target.parentElement.className === 'delete-item secondary-item') {
-  //   console.log('delete item');
-  // }
-
-  if (e.target.parentElement.classList.contains('delete-item')) {
-    console.log('delete item');
-    e.target.parentElement.parentElement.remove();
-  }
-}
+// get from Sotrage
+const name = localSotrage.getItem('name');
+console.log(name);
