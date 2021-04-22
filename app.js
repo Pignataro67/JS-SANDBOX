a0e570113dae6fe74836889c666a047114abce1d
Pignataro67 <Pignataro67@gmail.com>
2021-04-22 05:48:33 -0700

    adding function

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert(1);
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  console.log('Calculating...');
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthlyPayment');
+  const totalPayment = document.getElementById('totalPayment');
+  const totalInterest = document.getElementById('totalInterest');
+
+  const principal =  parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) /100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;;
+
+  e.preventDefault();
+}
